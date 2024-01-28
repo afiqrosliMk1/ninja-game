8eb23417bc027e0843d4445f5ee553c1dee34015
afiq acer v nitro <50091836+afiqrosliMk1@users.noreply.github.com>
2024-01-28 14:59:24 +0800

    implement collision and jumping

--- scripts/tilemap.py
+++ scripts/tilemap.py
@@ -1,3 +1,8 @@
+import pygame
+
+NEIGHBOR_OFFSETS = [(-1, 0), (-1, -1), (0, -1), (1, -1), (1, 0), (0, 0), (-1, 1), (0, 1), (1, 1)]
+PHYSICS_TILES = {'grass', 'stone'} #set - dict without value. fast
+
 class Tilemap:
     def __init__(self, game, tile_size=16):
         self.game = game
@@ -9,7 +14,25 @@ class Tilemap:
             self.tilemap[str(3 + i) + ';10'] = {'type': 'grass', 'variant': 1, 'pos': (3 + i, 10)}
             self.tilemap['10;' + str(i + 5)] = {'type': 'stone', 'variant': 1, 'pos': (10, 5 + i)}
 
+    def tiles_around(self, pos): 
+        tiles = []
+        tile_loc = (int(pos[0] // self.tile_size) , int(pos[1] // self.tile_size))
+        for offset in NEIGHBOR_OFFSETS:
+            check_loc = str(tile_loc[0] + offset[0]) + ';' + str(tile_loc[1] + offset[1])
+
+            if check_loc in self.tilemap:
+                tiles.append(self.tilemap[check_loc])
+        return tiles
+            
+    def physics_rects_around(self, pos):
+        rects = []
+        for tile in self.tiles_around(pos):
+            if tile['type'] in PHYSICS_TILES:
+                rects.append(pygame.Rect(tile['pos'][0] * self.tile_size, tile['pos'][1] * self.tile_size, self.tile_size, self.tile_size))
+        return rects
+
     def render(self, surf):
+        #offgrid tiles are at the back, so render it first v  
         for tile in self.offgrid_tiles:
             surf.blit(self.game.assets[tile['type']][tile['variant']], tile['pos'])
             
