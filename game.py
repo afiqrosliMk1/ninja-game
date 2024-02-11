c6ee5ad35227cc76c8c8fd5d7733e8eb5c8d6ad1
afiq acer v nitro <50091836+afiqrosliMk1@users.noreply.github.com>
2024-02-11 16:02:08 +0800

    add background image

--- game.py
+++ game.py
@@ -24,7 +24,8 @@ class Game:
             'grass': load_images('tiles/grass'),
             'large_decor': load_images('tiles/large_decor'),
             'stone': load_images('tiles/stone'),
-            'player': load_image('entities/player.png')
+            'player': load_image('entities/player.png'),
+            'background': load_image('background.png')
         }
 
         print(self.assets)
@@ -37,7 +38,7 @@ class Game:
 
     def run(self):
         while True:
-            self.display.fill((14 , 219 , 248 ))
+            self.display.blit(self.assets['background'], (0, 0))
 
             self.scroll[0] += (self.player.rect().centerx - self.display.get_width() / 2 - self.scroll[0]) / 30
             self.scroll[1] += (self.player.rect().centery - self.display.get_height() / 2 - self.scroll[1]) / 30
