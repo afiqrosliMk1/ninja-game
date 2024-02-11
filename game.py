27028d3c8ef8b0f65416c79246f976fe50260099
afiq acer v nitro <50091836+afiqrosliMk1@users.noreply.github.com>
2024-02-11 15:57:36 +0800

    add camera rolling math and int casting for offset to avoid jittering

--- game.py
+++ game.py
@@ -39,12 +39,14 @@ class Game:
         while True:
             self.display.fill((14 , 219 , 248 ))
 
-            self.scroll[0] += 1
+            self.scroll[0] += (self.player.rect().centerx - self.display.get_width() / 2 - self.scroll[0]) / 30
+            self.scroll[1] += (self.player.rect().centery - self.display.get_height() / 2 - self.scroll[1]) / 30
+            render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
 
-            self.tilemap.render(self.display, offset=self.scroll)
+            self.tilemap.render(self.display, offset=render_scroll)
 
             self.player.update( self.tilemap, (self.movement[1] - self.movement[0] , 0) )
-            self.player.render(self.display, offset=self.scroll)
+            self.player.render(self.display, offset=render_scroll)
 
             print(self.tilemap.physics_rects_around(self.player.pos))
 
