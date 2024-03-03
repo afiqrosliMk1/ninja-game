1684101b96cb5f26750bab77870d9a958b3d3057
afiq acer v nitro <50091836+afiqrosliMk1@users.noreply.github.com>
2024-03-03 16:41:33 +0800

    add 'save' function to export map from editor into json file

--- scripts/tilemap.py
+++ scripts/tilemap.py
@@ -1,3 +1,5 @@
+import json
+
 import pygame
 
 NEIGHBOR_OFFSETS = [(-1, 0), (-1, -1), (0, -1), (1, -1), (1, 0), (0, 0), (-1, 1), (0, 1), (1, 1)]
@@ -20,6 +22,11 @@ class Tilemap:
                 tiles.append(self.tilemap[check_loc])
         return tiles
             
+    def save(self, path):
+        f = open(path, 'w')
+        json.dump({'tilemap': self.tilemap, 'tile_size': self.tile_size, 'offgrid': self.offgrid_tiles}, f)
+        f.close
+
     def physics_rects_around(self, pos):
         rects = []
         for tile in self.tiles_around(pos):
