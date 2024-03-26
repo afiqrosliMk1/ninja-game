ff457cfeb3cae3165e4c557247dcfff48292f106
afiq acer v nitro <50091836+afiqrosliMk1@users.noreply.github.com>
2024-03-26 22:38:37 +0800

    cap the number of level to number of map we have

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import math
 import random
@@ -92,7 +93,7 @@ class Game:
             if not len(self.enemies):
                 self.transition += 1
                 if self.transition > 30:
-                    self.level += 1
+                    self.level = min(self.level + 1, len(os.listdir('data/maps')) - 1)
                     self.load_level(self.level)
             if self.transition < 0:
                 self.transition += 1
